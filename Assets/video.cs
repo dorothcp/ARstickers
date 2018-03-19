9056a9fa079e39fb3d110ffc6c3aa3f323e2d456
Christian Bigham - Desktop <cbbigham@usc.edu>
2018-03-18 20:36:58 -0700

    project commit

---
+++ Assets/video.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+[RequireComponent (typeof(AudioSource))]
+
+
+public class video : MonoBehaviour {
+
+	public MovieTexture movie;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		// this line of code will make the Movie Texture begin playing
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	
+
+
+
+	
+	}
+}
